8455efd7f92642c8c85535c0d2bb15471e83b106
James Vogenthaler <javo9205@colorado.edu>
2024-04-01 19:33:41 -0600

    Added usage info

--- bin2mem.sh
+++ bin2mem.sh
@@ -1,3 +1,6 @@
+# Short script to convert a binary into a hex .mem file for use with $memreadh
+# Usage: ./bin2mem filename
+
 FMT='2/2 "%04X" "\n"'
 if [ -z "$1" ]; then cat | hexdump -ve "$FMT"; else hexdump -ve "$FMT" "$1"; fi
 
